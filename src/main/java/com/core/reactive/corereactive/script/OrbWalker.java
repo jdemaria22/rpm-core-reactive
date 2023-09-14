5f911b202945a08a7f22883b3b133292512f08e7
Javier Peirone <peironej@gmail.com>
2023-09-14 18:48:25 -0300

    AiManager 100% Done.

--- src/main/java/com/core/reactive/corereactive/script/OrbWalker.java
+++ src/main/java/com/core/reactive/corereactive/script/OrbWalker.java
@@ -62,7 +62,6 @@ public class OrbWalker implements ScriptLoaderService {
                 .flatMap(jsonActivePlayer -> Mono.just(jsonActivePlayer.championStats.getAttackSpeed()))
                 .flatMap(attackSpeed -> {
                     Champion localPlayer =  championComponent.getLocalPlayer();
-                    log.info("aimanager:  {}", localPlayer.getAiManager());
                     Double range = (double) localPlayer.getAttackRange();
                     Double gameTime = this.gameTimeComponent.getGameTime();
                     return this.targetService.getBestChampionInRange(range)
